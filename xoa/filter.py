fb6441c5d62f95f67d5cfe59263b1105708dd12f
Stephane Raynaud <stephane.raynaud@shom.fr>
2020-10-22 11:27:45 +0200

    Add erode_mask

--- xoa/filter.py
+++ xoa/filter.py
@@ -5,19 +5,122 @@ import numpy as np
 import xarray as xr
 
 from .__init__ import XoaError
-from .coords import transpose
+from .coords import transpose, get_dims
 
 
-def generate_isotropic_kernel(shape, func, fill_value=0, npt=None):
+def get_window_func(window, *args, **kwargs):
+    """Get a window function from its name
+
+    Parameters
+    ----------
+    window: str, callable, list, array_like
+        Specification to get the window function:
+
+        - `callable`: used as is.
+        - `str`: supposed to be function name of the :mod:`numpy` or
+          :mod:`scipy.signal.windows` modules.
+        - `list`, `array_like`: transformed to an array and interpolated
+          onto ``size`` points.
+
+    args, kwargs:
+        Argument passed to the low level window function at calling time.
+
+    Return
+    ------
+    callable
+        A function that only a ``size`` argument
+
+    Example
+    -------
+    .. ipython:: python
+
+        @suppress
+        matplotlib.pyplot as plt
+        @suppress
+        from xoa.filter import get_window_func
+        func0 = get_window_func("gaussian", 22, sym=True)
+        func1 = get_window_func([1, 2, 5, 2, 1])
+        plt.plot(func0(100), label='Gaussian');
+        plt.plot(func1(100), label='List/array');
+        @savefig api.filter.get_window_func.png
+        plt.legend();
+    """
+    # Explicit values so create a wrapper that interpolate them
+    if isinstance(window, (list, np.ndarray)):
+        def window_func(size):
+            x = np.linspace(0, 1, size)
+            xp = np.linspace(0, 1, len(window))
+            return np.interp(x, xp, window)
+        return window_func
+
+    # Base function
+    if isinstance(window, str):
+        if hasattr(np, window):
+            func = getattr(np, window)
+        else:
+            import scipy.signal.windows as sw
+            func = getattr(sw, window, None)
+            if func is None:
+                raise XoaError(f'Invalid window name: {window}')
+    else:
+        func = window
+
+    # Wrapper with args
+    if args or kwargs:
+        def window_func(size):
+            return func(size, *args, **kwargs)
+        return window_func
+    return func
+
+
+# def get_window_kernel(size, window, *args, **kwargs):
+#     """Generate an 1d kernel from a window name or function
+
+#     It first get the window function, then returns::
+
+#         window_func(size)
+
+#     Parameters
+#     ----------
+#     size: int
+#         Size of the output kernel
+#     window: str, callable, list, array_like
+#         Specification to get the window function:
+
+#         - `callable`: used as is.
+#         - `str`: supposed to be function name of the :mod:`numpy` or
+#           :mod:`scipy.signal.windows` modules.
+#         - `list`, `array_like`: transformed to an array and interpolated
+#           onto ``size`` points.
+
+#     Return
+#     ------
+#     np.ndarray
+#         1D kernel
+
+#     See also
+#     --------
+#     get_window_func
+#     """
+#     if isinstance(window, str):
+#         window = get_window_func(window, *args, **kwargs)
+#     elif isinstance(window, (list, np.ndarray)):
+#         x = np.linspace(0, 1, size)
+#         xp = np.linspace(0, 1, len(window))
+#         return np.interp(x, xp, window)
+#     return window(size)
+
+
+def generate_isotropic_kernel(shape, window_func, fill_value=0, npt=None):
     """Generate an nD istropic kernel given a shape and a window function
 
     Parameters
     ----------
-    func: str, callable
-        Function that take a size parameter as unique argument.
-        If a string, it is expected to be a numpy window function.
     shape: int, tuple
         Shape of the desired kernel
+    window_func: str, callable
+        Function that take a size parameter as unique argument.
+        If a string, it is expected to be a numpy window function.
     fill_value: float
         Value to set when outside window bounds, near the corners
     npt: int, None
@@ -26,8 +129,8 @@ def generate_isotropic_kernel(shape, func, fill_value=0, npt=None):
 
     Return
     ------
-    array_like(shape)
-        Output kernel
+    np.ndarray
+        Isotropic kernel array
 
     Example
     -------
@@ -37,14 +140,15 @@ def generate_isotropic_kernel(shape, func, fill_value=0, npt=None):
         import numpy as np, matplotlib.pyplot as plt
         @suppress
         from xoa.filter import generate_isotropic_kernel
-        kernel = generate_isotropic_kernel((150, 200), "bartlett", np.nan)
-        plt.axes(aspect=1);
-        plt.pcolormesh(kernel, cmap="cmo.solar");
+        kernel = generate_isotropic_kernel((20, 30), "bartlett", np.nan)
+        plt.matshow(kernel, cmap="cmo.solar");
         @savefig api.filter.generate_isotropic_kernel.png
         plt.colorbar();
 
     See also
     --------
+    generate_orthogonal_kernel
+    generate_kernel
     numpy.bartlett
     numpy.blackman
     numpy.hamming
@@ -53,9 +157,8 @@ def generate_isotropic_kernel(shape, func, fill_value=0, npt=None):
     numpy.interp
 
     """
-    # Windox function
-    if isinstance(func, str):
-        func = getattr(np, func)
+    # Window function
+    window_func = get_window_func(window_func)
 
     # Normalised indices
     indices = np.indices(shape).astype('d')
@@ -69,7 +172,7 @@ def generate_isotropic_kernel(shape, func, fill_value=0, npt=None):
     # Window values
     if npt is None:
         npt = 2*max(shape)
-    fp = func(npt)
+    fp = window_func(npt)
     xp = np.linspace(0, 1, npt)
 
     # Interpolation
@@ -79,13 +182,106 @@ def generate_isotropic_kernel(shape, func, fill_value=0, npt=None):
     return kernel
 
 
-def generate_kernel(kernel, data):
+def generate_orthogonal_kernel(kernels, window_func="ones", fill_value=0.):
+    """Generate an nD kernel from orthogonal 1d kernels
+
+    Parameters
+    ----------
+    kernels: list
+        List of scalars and/or 1d kernels.
+        In case of a scalar, it is converted to an 1d kernel with
+        the ``window_func`` parameter.
+    window_func: str, callable
+        Function that take a size parameter as unique argument.
+        If a string, it is expected to be a numpy window function.
+    fill_value: float
+        Value to set when outside window bounds, when creating an 1d kernel
+        from a floating size
+
+    Return
+    ------
+    np.ndarray
+        Orthogonal kernel array
+
+    Example
+    -------
+    .. ipython:: python
+
+        @suppress
+        import numpy as np, matplotlib.pyplot as plt
+        @suppress
+        from matplotlib.gridspec import GridSpec
+        @suppress
+        from xoa.filter import generate_orthogonal_kernel
+        # From sizes and functions
+        ny, nx = (21, 31)
+        kernel = generate_orthogonal_kernel((ny, nx), "bartlett")
+        j, i = 5, 20
+        fig = plt.figure(constrained_layout=True)
+        gs = GridSpec(3, 3, figure=fig)
+        ax0 = fig.add_subplot(gs[1:, :2])
+        ax0.matshow(kernel)
+        ax0.axhline(j, color='tab:red')
+        ax0.axvline(i, color='tab:orange')
+        ax1 = fig.add_subplot(gs[0, :2], sharex=ax0)
+        ax1.plot(np.arange(nx), kernel[j], color='tab:red')
+        ax2 = fig.add_subplot(gs[1:, -1], sharey=ax0)
+        ax2.plot(kernel[:, i], np.arange(ny), color='tab:orange')
+        @savefig api.filter.generate_orthogonal_kernel_0.png
+        plt.colorbar();
+        # From 1d kernels
+        kernel = xfilter.generate_orthogonal_kernel(
+            ([1, 1, 1], [1, 2, 3, 2, 1]))
+        plt.figure();
+        plt.matshow(kernel);
+        @savefig api.filter.generate_orthogonal_kernel_1.png
+
+
+    See also
+    --------
+    generate_isotropic_kernel
+    generate_kernel
+    numpy.bartlett
+    numpy.blackman
+    numpy.hamming
+    numpy.hanning
+    numpy.kaiser
+    numpy.interp
+
+    """
+    kernel = None
+    for k1d in kernels:
+
+        # From scalar to 1d
+        if np.isscalar(k1d):
+            window_func = get_window_func(window_func)
+            if isinstance(k1d, int):
+                k1d = window_func(k1d)
+            else:  # Float case
+                size = int(k1d)
+                if size != k1d:
+                    size += 2
+                k1d = np.full(k1d, fill_value)
+                k1d[1:-1] = window_func(size-2)
+        else:
+            k1d = np.asarray(k1d)
+
+        # Orthogonal merge
+        if kernel is None:
+            kernel = k1d
+        else:
+            kernel = np.tensordot(kernel[:, None], k1d[None], axes=1)
+
+    return kernel
+
+
+def generate_kernel(
+        kernel, data, isotropic=False, window_func="ones", fill_value=0.,
+        window_args=None, window_kwargs=None):
     """Generate a kernel that is compatible with a given data array
 
     Parameters
     ----------
-    data: xarray.DataArray
-        Data array that the kernel must be compatible with
     kernel: xarray.DataArray, np.ndarray, int, list, dict
         Ready to use kernel or specs to generate it.
 
@@ -97,6 +293,8 @@ def generate_kernel(kernel, data):
 
         The final data array is transposed and/or expanded with
         :func:`xoa.coords.transpose` to fit into the input data array.
+    data: xarray.DataArray
+        Data array that the kernel must be compatible with
 
     Return
     ------
@@ -106,45 +304,97 @@ def generate_kernel(kernel, data):
     See also
     --------
     generate_isotropic_kernel
+    generate_orthogonal_kernel
     xoa.coords.transpose
     """
-    # Constant kernel of the given size
+    # Isotropic
+    if isotropic is True:
+        isotropic = data.dims
+
+    # Convert to tuple
     if isinstance(kernel, int):
         kernel = (kernel,)*data.ndim
 
-    # Constant kernel of the given sizes
+    # Convert to dict
     if isinstance(kernel, tuple):
         if len(kernel) > data.ndim:
             raise XoaError("Too many dimensions for your kernel: {} > {}"
                            .format(len(kernel), data.ndim))
-        kernel = np.ones(kernel, dtype=data.dtype)
-        dims = data.dims[-kernel.ndim:]
+        kernel = dict(item for item in
+                      zip(data.dims[-len(kernel):], kernel))
 
-    # From a single 1d kernel
-    elif isinstance(kernel, list):
-        kernel = np.array(kernel)
-        k1d = kernel
-        while kernel.ndim < data.ndim:
-            kernel = np.tensordot(k1d[:, None], kernel[None], axes=1)
-        dims = data.dims
+    # Convert to dict
+    if isinstance(kernel, list):
+        kernel = dict((dim, kernel) for dim in data.dims)
 
-    # From an 1d kernel for given dimensions
-    elif isinstance(kernel, dict):
-        kd = kernel
-        for dim, k1d in kd.items():
-            k1d = np.array(k1d)
-            if dim not in data.dims:
-                raise XoaError(f"invalid dimension for your kernel: {dim}")
-            if kernel is kd:
-                kernel = k1d
+    # From an size or 1d kernel for given dimensions
+    if isinstance(kernel, dict):
+
+        # Isotropic parameter
+        if isotropic:
+            if isotropic is True:
+                isotropic = data.dims
+            elif not set(isotropic).issubset(data.dims):
+                raise XoaError("invalid dimensions for isotropic keyword")
+
+        # Split into consistant isotropic and orthogonal kernels
+        isokernels_sizes = {}
+        isokernel = None
+        orthokernels = {}
+        for dim, kn in kernel.items():
+            if isotropic and dim not in isotropic:
+                orthokernels[dim] = kn
             else:
-                kernel = np.tensordot(kernel[:, None], k1d[None], axes=1)
-        dims = list(kd.keys())
+                if isokernel:
+                    if ((np.isscalar(isokernel) and not np.isscalar(kn)) or
+                            (not np.isscalar(isokernel) and np.isscalar(kn))):
+                        raise XoaError("Inconsistant mix of 1d and scalar "
+                                       "kernel specs for building isotropic "
+                                       "kernel")
+                    if (not np.isscalar(kn) and not np.isscalar(isokernel)
+                            and not np.allclose(kn, isokernel)):
+                        raise XoaError("Inconsistant 1d kernels for building "
+                                       "isotropic kernel")
+                else:
+                    isokernel = kn
+                size = kn if np.isscalar(kn) else len(kn)
+                isokernels_sizes[dim] = size
+
+        # Merge orthogonal kernels
+        dims = ()
+        kernel = None
+        if orthokernels:
+            dims += tuple(orthokernels.keys())
+            window_args = [] if window_args is None else window_args
+            window_kwargs = {} if window_kwargs is None else window_kwargs
+            window_func = get_window_func(
+                window_func, *window_args, **window_kwargs)
+            kernel = generate_orthogonal_kernel(
+                orthokernels, window_func=window_func, fill_value=fill_value)
+
+        # Build isotropic kernel
+        if isokernel:
+
+            # List/array
+            if not np.isscalar(isokernel):
+                window_func = get_window_func(isokernel)
+
+            # nD isotropic kernel
+            isokernels = generate_isotropic_kernel(
+                tuple(isokernels_sizes.values()), window_func,
+                fill_value=fill_value)
+
+            # Update final kernel
+            dims += tuple(isokernels_sizes.keys())
+            if kernel is None:
+                kernel = isokernels
+            else:
+                kernel = np.tensordot(kernel[..., None], isokernels[None, ...])
 
     # Numpy
     elif isinstance(kernel, np.ndarray):
         if kernel.ndim > data.ndim:
-            raise XoaError("Too many dimensions for your numpy kernel: {} > {}"
+            raise XoaError("too many dimensions for your numpy kernel: {} > {}"
                            .format(kernel.dim, data.ndim))
         dims = data.dims[-kernel.ndim:]
 
@@ -157,6 +407,42 @@ def generate_kernel(kernel, data):
 
     # Finalize
     return transpose(kernel, data, mode="insert").astype(data.dtype)
+
+
+def shapiro_kernel(dims):
+    """Generate a shapiro kernel
+
+    Parameters
+    ----------
+    dims: str, tuple
+        Dimension names
+
+    Return
+    ------
+    xarray.DataArray
+        The kernel as a data array with provided dims and a shape of
+        ``(3,)*len(dims)``
+
+    Example
+    -------
+    .. ipython:: python
+
+        @suppress
+        from xoa.filter import shapiro
+        shapiro('nx')
+        shapiro(('ny', 'nx'))
+        shapiro(('nt', 'ny', 'nx'))
+
+    """
+    if isinstance(dims, str):
+        dims = (dims, )
+    ndim = len(dims)
+    kernel = np.zeros((3,)*ndim, dtype='d')
+    indices = np.indices(kernel.shape)
+    for idx in indices:
+        idx[idx == 2] = 0
+        kernel += idx
+    return xr.DataArray(kernel, dims=dims)
 
 
 def _convolve_(data, kernel, normalize):
@@ -190,7 +476,7 @@ def _convolve_(data, kernel, normalize):
 
 
 def convolve(data, kernel, normalize=False):
-    """N-dimensional convolution that take care of nans
+    """N-dimensional convolution that takes care of nans
 
     Parameters
     ----------
@@ -242,17 +528,71 @@ def convolve(data, kernel, normalize=False):
     return xr.DataArray(datac, coords=data.coords, attrs=data.attrs)
 
 
-# def erode_mask(data, until=1, kernel=3):
+def erode_mask(data, until=1, kernel=None, xdim=None, ydim=None):
+    """Erode the horizontal mask using smoothing
 
-#     if isinstance(until, int):
-#         niter = until
-#         mask = None
-#     else:
-#         mask = until
-#         # if mask.shape != data.shape
+    Parameter
+    ---------
+    data: xarray.DataArray
+        Array of at least 2 dimensions, that are supposed to be horizontal.
+    until: array_like, int
+        Either a 2D minimal mask, or a max number of iteration.
+    kernel:
+        Defaults to a :func:`shapiro <shapiro_kernel>` kernel.
+        In this case, ``xdim`` and ``ydim`` can be set to the
+        horizontal dimensions, otherwise they are inferred.
+        If ``kernel`` is provided, it must a compatible with
+        :func:`generate_kernel`.
 
-#     for i in range(niter):
-#         datac = xr_filternd(data, kernel)
-#         data = xr.where(np.isnan(data), datac, data)
-#     return data
+    Return
+    xarray.DataArray
+        Data array similar to input array, with its eroded
+        along x and y dimensions.
+    """
+
+    if data.dim < 2:
+        raise XoaError("input array must have at least 2 dimensions")
+
+    # Iteration or mask
+    if isinstance(until, int):
+        niter = until
+        mask = None
+    else:
+        mask = until
+        if isinstance(mask, np.ndarray):
+            if mask.ndim > 2:
+                raise XoaError("mask must be two-dimensional")
+            if xdim is None:
+                xdim = get_dims(
+                    data, 'x', allow_positional=True, errors="raise")
+            if ydim is None:
+                ydim = get_dims(
+                    data, 'y', allow_positional=True, errors="raise")
+            mask = xr.DatArray(mask, dims=(ydim, xdim))
+        if not set(mask.dims).issubset(data.dims):
+            raise XoaError('mask dims must be a subset of data dims')
+        mask = transpose(kernel, data, mode="compat")
+
+    # Kernel
+    if kernel is None:
+        if xdim is None:
+            xdim = get_dims(data, 'x', allow_positional=True, errors="raise")
+        if ydim is None:
+            ydim = get_dims(data, 'y', allow_positional=True, errors="raise")
+        kernel = shapiro((ydim, xdim))
+    kernel = generate_kernel(kernel, data)
+    if mask.ndim > 2:
+        raise XoaError("kernel must be two-dimensional")
+
+    # Filter
+    if mask is None:
+        def to2dmask(data):
+            mask2d = np.isnan(data.values).
+            for dim in
+        while
+
+    for i in range(niter):
+        datac = convolve(data, kernel)
+        data = xr.where(np.isnan(data), datac, data)
+    return data
 
