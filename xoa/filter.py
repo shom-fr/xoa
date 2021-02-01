Fix convolve with masking

xoa/filter.py
@@ -439,7 +439,7 @@ def generate_kernel(
     elif not set(kernel.dims).issubset(set(data.dims)):
         raise XoaError(f"kernel dimensions {kernel.dims} "
                        f"are not a subset of {data.dims}")
-
+      
     # Finalize
     return transpose(kernel, data, mode="insert").astype(data.dtype)
 
@@ -486,6 +486,11 @@ def _convolve_(data, kernel, normalize):
 
     # Kernel
     assert data.ndim == kernel.ndim
+    if kernel.dtype is not data.dtype:
+        xoa_warn(
+            "The dtype of your kernel is not the same as that of your data. "
+            "Converting it...")
+        kernel = kernel.astype(data.dtype)
 
     # Guess mask
     bad = np.isnan(data)
@@ -501,7 +506,7 @@ def _convolve_(data, kernel, normalize):
 
     # Weigthing and masking
     if with_mask:
-        bad = np.isclose(weights, 0)
+        bad = np.isclose(weights, 0, atol=float(1e-6*kernel.sum()))
     if normalize:
         if with_mask:
             weights[bad] = 1
@@ -558,7 +563,7 @@ def convolve(data, kernel, normalize=False):
     kernel = generate_kernel(kernel, data)
 
     # Numpy convolution
-    datac = _convolve_(data, kernel.data, normalize)
+    datac = _convolve_(data.data, kernel.data, normalize)
 
     # Format
     return xr.DataArray(datac, coords=data.coords, attrs=data.attrs)