b3a80aa7064a255523348468618fb5a3ae769a19
Stephane Raynaud <stephane.raynaud@shom.fr>
2021-02-18 13:54:09 +0100

    Add get_order

--- xoa/coords.py
+++ xoa/coords.py
@@ -2,36 +2,19 @@
 """
 Coordinates and dimensions utilities
 """
-# Copyright or © or Copr. Shom, 2020
+# Copyright 2020-2021 Shom
 #
-# This software is a computer program whose purpose is to [describe
-# functionalities and technical features of your software].
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
 #
-# This software is governed by the CeCILL license under French law and
-# abiding by the rules of distribution of free software.  You can  use,
-# modify and/ or redistribute the software under the terms of the CeCILL
-# license as circulated by CEA, CNRS and INRIA at the following URL
-# "http://www.cecill.info".
+#     http://www.apache.org/licenses/LICENSE-2.0
 #
-# As a counterpart to the access to the source code and  rights to copy,
-# modify and redistribute granted by the license, users are provided only
-# with a limited warranty  and the software's author,  the holder of the
-# economic rights,  and the successive licensors  have only  limited
-# liability.
-#
-# In this respect, the user's attention is drawn to the risks associated
-# with loading,  using,  modifying and/or developing or reproducing the
-# software by the user in light of its specific status of free software,
-# that may mean  that it is complicated to manipulate,  and  that  also
-# therefore means  that it is reserved for developers  and  experienced
-# professionals having in-depth computer knowledge. Users are therefore
-# encouraged to load and test the software's suitability as regards their
-# requirements in conditions enabling the security of their systems and/or
-# data to be ensured and,  more generally, to use and operate it in the
-# same conditions as regards security.
-#
-# The fact that you are presently reading this means that you have had
-# knowledge of the CeCILL license and that you accept its terms.
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 from collections.abc import Mapping
 
@@ -486,7 +469,7 @@ def get_dim_types(dims, unknown=None, asdict=False):
     dims: tuple(str) or xarray.DataArray
         Dimensions or data array
     unknown:
-        Vaue to assign to unknown types
+        Value to assign to unknown types
     asdict: bool
         Get the result as dictionary
 
@@ -502,6 +485,11 @@ def get_dim_types(dims, unknown=None, asdict=False):
     cfspecs = cf.get_cf_specs()
     return cfspecs.coords.get_dim_types(
         dims, da=da, unknown=unknown, asdict=asdict)
+
+
+def get_order(da):
+    """Like :func:`get_dim_types` but returning a string"""
+    return "".join(get_dim_types(da, unknown=="-", asdict=False))
 
 
 def reorder(da, order):
@@ -545,3 +533,4 @@ def reorder(da, order):
 
     # Final transpose
     return transpose(da, to_dims)
+
