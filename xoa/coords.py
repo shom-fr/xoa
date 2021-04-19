6d15ecf169266c02305614ddd4f2fb99da8f0f23
Stephane Raynaud <stephane.raynaud@shom.fr>
2021-04-19 11:31:14 +0200

    Add get_x/y/z/t/f_dim

--- xoa/coords.py
+++ xoa/coords.py
@@ -160,8 +160,7 @@ def get_cf_coords(da, coord_names, errors="raise"):
 
 
 @misc.ERRORS.format_function_docstring
-def get_dims(da, dim_types, allow_positional=False, positions='tzyx',
-             errors="warn"):
+def get_dims(da, dim_types, allow_positional=False, positions='tzyx', errors="warn"):
     """Get the data array dimensions names from their type
 
     Parameters
@@ -188,6 +187,156 @@ def get_dims(da, dim_types, allow_positional=False, positions='tzyx',
     return cf.get_cf_specs(da).get_dims(
         da, dim_types, allow_positional=allow_positional,
         positions=positions, errors=errors)
+
+
+@misc.ERRORS.format_function_docstring
+def get_xdim(da, errors="warn", **kwargs):
+    """Get the dimension of X type
+
+    It is a simple call to :func:`get_dims` with ``dim_types="x"``
+
+    Parameters
+    ----------
+    da: xarray.DataArray
+        Array to scan
+    positions: str
+        Default position per type starting from the end.
+    {errors}
+    kwargs: dict
+        Extra parameters are passed to :func:`get_dims`
+
+    Return
+    ------
+    str or None
+        The dimension name or None
+
+    See also
+    --------
+    get_dims
+    """
+    dims = get_dims(da, "x", errors=errors)
+    if dims:
+        return dims[0]
+
+
+@misc.ERRORS.format_function_docstring
+def get_ydim(da, errors="warn", **kwargs):
+    """Get the dimension of Y type
+
+    It is a simple call to :func:`get_dims` with ``dim_types="y"``
+
+    Parameters
+    ----------
+    da: xarray.DataArray
+        Array to scan
+    positions: str
+        Default position per type starting from the end.
+    {errors}
+    kwargs: dict
+        Extra parameters are passed to :func:`get_dims`
+
+    Return
+    ------
+    str or None
+        The dimension name or None
+
+    See also
+    --------
+    get_dims
+    """
+    dims = get_dims(da, "y", errors=errors)
+    if dims:
+        return dims[0]
+
+
+@misc.ERRORS.format_function_docstring
+def get_zdim(da, errors="warn", **kwargs):
+    """Get the dimension of Z type
+
+    It is a simple call to :func:`get_dims` with ``dim_types="z"``
+
+    Parameters
+    ----------
+    da: xarray.DataArray
+        Array to scan
+    positions: str
+        Default position per type starting from the end.
+    {errors}
+    kwargs: dict
+        Extra parameters are passed to :func:`get_dims`
+
+    Return
+    ------
+    str or None
+        The dimension name or None
+
+    See also
+    --------
+    get_dims
+    """
+    dims = get_dims(da, "z", errors=errors)
+    if dims:
+        return dims[0]
+
+
+@misc.ERRORS.format_function_docstring
+def get_tdim(da, errors="warn", **kwargs):
+    """Get the dimension of T type
+
+    It is a simple call to :func:`get_dims` with ``dim_types="t"``
+
+    Parameters
+    ----------
+    da: xarray.DataArray
+        Array to scan
+    positions: str
+        Default position per type starting from the end.
+    {errors}
+    kwargs: dict
+        Extra parameters are passed to :func:`get_dims`
+
+    Return
+    ------
+    str or None
+        The dimension name or None
+
+    See also
+    --------
+    get_dims
+    """
+    dims = get_dims(da, "t", errors=errors)
+    if dims:
+        return dims[0]
+
+
+@misc.ERRORS.format_function_docstring
+def get_fdim(da, errors="warn", **kwargs):
+    """Get the dimension of F type
+
+    It is a simple call to :func:`get_dims` with ``dim_types="f"``
+
+    Parameters
+    ----------
+    da: xarray.DataArray
+        Array to scan
+    positions: str
+        Default position per type starting from the end.
+    {errors}
+    kwargs: dict
+        Extra parameters are passed to :func:`get_dims`
+
+    Return
+    ------
+    str or None
+        The dimension name or None
+
+    See also
+    --------
+    get_dims
+    """
+    dims = get_dims(da, "f", errors=errors)
+    if dims:
+        return dims[0]
 
 
 class transpose_modes(misc.IntEnumChoices, metaclass=misc.DefaultEnumMeta):
@@ -304,162 +453,6 @@ def transpose(da, dims, mode='compat'):
 
 
 transpose.__doc__ = transpose.__doc__.format(**locals())
-
-
-class DimFlusher1D(object):
-
-    def __init__(self, da_in, coord_out, dim=None, coord_in_name=None):
-        """Right-flush the working dimension
-
-        Parameters
-        ----------
-        da_in: xarray.DataArray
-            Input data array
-        coord_out: xarray.DataArray
-            Output coordinate array
-        dim: str, tuple, None
-            Working dimension
-        coord_in_name: str, None
-            Input coordinate name. If not provided, it is infered.
-        """
-        # Get the working dimensions
-        if not isinstance(dim, (tuple, list)):
-            dim = (dim, dim)
-        dim0, dim1 = dim
-        if None in dim or coord_in_name is None:
-            cfspecs = cf.get_cf_specs(da_in)
-        # - dim1 (out)
-        if dim1 is None:  # get dim1 from coord_out
-            dim1 = cfspecs.search_dim(coord_out)
-            if dim1 is None:
-                raise cf.XoaCFError("No CF dimension found for output coord. "
-                                    "Please specifiy the working dimension.")
-            dim1, dim_type = dim1
-        else:  # dim1 is provided
-            dim_type = cfspecs.coords.get_dim_type(dim1, coord_out)
-        # - dim0 (in)
-        if dim0 is None:
-            if dim_type:
-                for c0 in da_in.coords.values():
-                    dim0 = cfspecs.coords.search_dim(c0, dim_type)
-                    if dim0:
-                        if dim_type is None:
-                            dim0 = dim0[0]
-                        break
-                else:
-                    raise cf.XoaCFError(
-                        "No CF {dim_type }dimension found for datarray. "
-                        "Please specifiy the working dimension.")
-            else:
-                dim0 = dim1  # be cafeful, dim1 must be in input!
-        assert dim0 in da_in.dims
-        assert dim1 in coord_out.dims
-
-        # Input coordinate
-        if coord_in_name:
-            assert coord_in_name in da_in.coords, 'Invalid coordinate'
-        else:
-            coord_in = cfspecs.search_coord_from_dim(da_in, dim0)
-            if coord_in is None:
-                raise cf.XoaCFError(
-                    f"No coordinate found matching dimension '{dim0}'")
-            coord_in_name = coord_in.name
-
-        # Check dims
-        # - non-common other dimensions
-        odims0 = set(da_in.dims).difference({dim0})
-        odims1 = set(coord_out.dims).difference({dim1})
-        if odims0.difference(odims1) and odims1.difference(odims0):
-            raise XoaError("Conflicting non working dimensions")
-        # - common dims, with size checking
-        cdims = odims0.intersection(odims1).difference({dim0})
-        for cdim in cdims:
-            assert da_in.sizes[cdim] == coord_out.sizes[cdim]
-        # - common dims in the order of da_in
-        cdims0 = []
-        for cdim0 in da_in.dims:
-            if cdim0 in cdims:
-                cdims0.append(cdim0)
-        # - input dims with output dim
-        dims01 = list(da_in.dims)
-        if dim0 != dim1:
-            dims01[dims01.index(dim0)] = dim1
-        dims01 = tuple(dims01)
-
-        # Store
-        self._dim0, self._dim1 = dim0, dim1
-        self._da_in = da_in
-        self.coord_out = transpose(coord_out, (Ellipsis,) + dims01, "compat")
-        self.coord_out_name = self.coord_out.name or coord_in.name
-        # self._odims0 = odims0
-        # self._odims1 = odims1
-        # self._cdims0 = cdims0
-        self.da_in = da_in
-
-        # Transpose to push work dim right
-        da_in = da_in.transpose(
-            Ellipsis, *(cdims0 + [self._dim0]), transpose_coords=True)
-        coord_out = coord_out.transpose(
-            Ellipsis, *(cdims0 + [self._dim1]))
-
-        # Broadcast data array
-        # - data var
-        if set(da_in.dims[:-1]) < set(coord_out.dims[:-1]):
-            da_in = da_in.broadcast_like(coord_out,
-                                         exclude=(self._dim0, self._dim1))
-        # - input coordinate
-        # if (set(coord_out.dims[:-1])set(da_in.coords[coord_in_name].dims[:-1])
-        #         < set(coord_out.dims[:-1])):
-        if (coord_out.ndim > 1 and set(coord_out.dims[:-1]) not in
-                set(da_in.coords[coord_in_name].dims[:-1])):
-
-        #set(coord_out.dims[:-1]) > set(da_in.coords[coord_in_name].dims[:-1]):
-            if da_in.coords[coord_in_name].ndim == 1:
-                coord_in_name, old_coord_in_name = (
-                    coord_in_name + '_dimflush1d', coord_in_name)
-            else:
-                old_coord_in_name = coord_in_name
-            da_in.coords[coord_in_name] = (
-                da_in.coords[old_coord_in_name].broadcast_like(
-                    coord_out, exclude=(self._dim0, self._dim1)))
-        coord_in = da_in.coords[coord_in_name]
-        # - output coordinate
-        if (coord_out.ndim > 1 and
-                set(coord_in.dims[:-1]) > set(coord_out.dims[:-1])):
-            coord_out = coord_out.broadcast_like(
-                coord_in, exclude=(self._dim0, self._dim1))
-
-        # Info reverse transfoms
-        # - input coords that doesn't have dim0 inside and must copied
-        self.extra_coords = dict([(name, coord) for name, coord
-                                  in da_in.coords.items()
-                                  if dim0 not in coord.dims])
-        # - da shape after reshape + broadcast
-        self.work_shape = da_in.shape[:-1] + (coord_out.sizes[dim1], )
-        self.work_dims = da_in.dims[:-1] + (dim1, )
-        self.final_dims = list(self._da_in.dims)
-        idim0 = self.final_dims.index(dim0)
-        self.final_dims[idim0] = dim1
-        self.final_dims = tuple(self.final_dims)
-        # self.final_shape = list(self._da_in.shape)
-        # self.final_shape[idim0] = coord_out.sizes[dim1]
-
-        # Convert to numpy 2D
-        self.da_in_data = da_in.data.reshape(-1, da_in.shape[-1])
-        self.coord_in_data = coord_in.data.reshape(-1, coord_in.shape[-1])
-        self.coord_out_data = coord_out.data.reshape(-1, coord_out.shape[-1])
-
-    def get_back(self, data_out):
-
-        data_out = data_out.reshape(self.work_shape)
-        da_out = xr.DataArray(data_out, dims=self.work_dims)
-        da_out = da_out.transpose(Ellipsis, *self.final_dims)
-        da_out[self.coord_out_name] = self.coord_out
-        da_out.attrs.update(self.da_in.attrs)
-        da_out.coords.update(self.extra_coords)
-        da_out.name = self.da_in.name
-
-        return da_out
 
 
 def get_dim_types(da, unknown=None, asdict=False):
