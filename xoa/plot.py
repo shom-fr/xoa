b65817d01d0faf754658e5c202c541695f9ff497
Stephane Raynaud <stephane.raynaud@shom.fr>
2022-02-02 15:11:56 +0100

    Add the plot_ts function
    Fix

--- xoa/plot.py
+++ xoa/plot.py
@@ -20,14 +20,28 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.colors as mcolors
 import matplotlib.collections as mcollections
+import xarray as xr
 
+from . import misc as xmisc
 from . import geo as xgeo
+from . import cf as xcf
+from . import coords as xcoords
 from . import dyn
 
 
 def plot_flow(
-        u, v, duration=None, step=None, particles=2000, axes=None,
-        alpha=(.2, 1), linewidth=.3, color="k", autolim=None, **kwargs):
+    u,
+    v,
+    duration=None,
+    step=None,
+    particles=2000,
+    axes=None,
+    alpha=(0.2, 1),
+    linewidth=0.3,
+    color="k",
+    autolim=None,
+    **kwargs,
+):
     """Plot currents as a windy-like plot with random little lagrangian tracks
 
     Parameters
@@ -60,19 +74,22 @@ def plot_flow(
 
     Return
     ------
-    matplotlib.collections.LineCollection
-        The duration and step are set as attributes with values in seconds.
+    Return
+    ------
+    dict
+        With the following key, depending on what is plotted:
+        `axes`, `lc`, `step`, `duration`.
+        `lc` is the :class:`matplotlib.collections.LineCollection` instance.
+        The duration and step are also stored in the output.
 
     Example
     ------
-
     .. ipython:: python
 
         @suppress
         import numpy as np, xarray as xr
         @suppress
         from xoa.plot import plot_flow
-
         # Setup data
         x = np.linspace(0, 2*np.pi, 20)
         y = np.linspace(0, 2*np.pi, 20)
@@ -86,9 +103,8 @@ def plot_flow(
             coords={"lat": ("lat", y), "lon": ("lon", x)})
 
         # Plot
-        lc = plot_flow(ds["u"], ds["v"])
         @savefig api.plot.plot_flow.png
-        lc.duration, lc.step
+        plot_flow(ds["u"], ds["v"])
 
     See also
     --------
@@ -102,8 +118,8 @@ def plot_flow(
         if duration is None:
             xmin, xmax, ymin, ymax = xgeo.get_extent(u)
             dist = xgeo.haversine(xmin, ymin, xmax, ymax)
-            speed = np.sqrt(np.abs(u.values).mean()**2+np.abs(v.values).mean()**2)
-            duration = dist/speed/30
+            speed = np.sqrt(np.abs(u.values).mean() ** 2 + np.abs(v.values).mean() ** 2)
+            duration = dist / speed / 30
         elif isinstance(duration, np.timedelta64):
             duration /= np.timedelta64(1, "s")
 
@@ -133,15 +149,17 @@ def plot_flow(
     segments = []
     linewidths = []
     colors = []
-    for j in range(tx.shape[0]-1):
+    for j in range(tx.shape[0] - 1):
         for i in range(tx.shape[1]):
-            segments.append(((tx[j, i], ty[j, i]), (tx[j+1, i], ty[j+1, i])))
+            segments.append(((tx[j, i], ty[j, i]), (tx[j + 1, i], ty[j + 1, i])))
             if not np.isscalar(alpha):
-                colors.append(color+(alpha[0]+cdists[j, i]*(alpha[-1]-alpha[0]),))
+                colors.append(color + (alpha[0] + cdists[j, i] * (alpha[-1] - alpha[0]),))
             else:
-                colors.append(color+(alpha,))
+                colors.append(color + (alpha,))
             if not np.isscalar(linewidth):
-                linewidths.append(linewidth[0]+cdists[j, i]*(linewidth[-1]-linewidth[0]),)
+                linewidths.append(
+                    linewidth[0] + cdists[j, i] * (linewidth[-1] - linewidth[0]),
+                )
             else:
                 linewidths.append(linewidth)
 
@@ -149,16 +167,164 @@ def plot_flow(
     kwargs.setdefault("colors", colors)
     kwargs.setdefault("linewidths", linewidths)
     lc = mcollections.LineCollection(segments, **kwargs)
-    lc.step = step
-    lc.duration = duration
+    out = {"step": step, "duration": duration}
     newaxes = axes is None
     if newaxes:
         axes = plt.gca()
     if autolim is None:
         autolim = newaxes
     axes.add_collection(lc, autolim=autolim)
+    out["axes"] = axes
 
-    return lc
+    return out
 
 
+def plot_ts(
+    temp,
+    sal,
+    dens=True,
+    potential=None,
+    axes=None,
+    scatter_kwargs=None,
+    contour_kwargs=None,
+    clabel=True,
+    clabel_kwargs=None,
+    colorbar=None,
+    colorbar_kwargs=None,
+    **kwargs,
+):
+    """Plot a TS diagram
 
+    A TS diagram is a scatter plot with absolute salinity as X axis
+    and potential temperature as Y axis.
+    The density is generally plotted as background contours.
+
+    Parameters
+    ----------
+    temp: xarray.DataArray
+        Temperature. If not potential temperature, please set `potential=True`.
+    sal: xarray.DataArray
+        Salinity
+    dens: bool
+        Add contours of density.
+        The density is computed with function :func:`gsw.density.sigma0`.
+    potential: bool, None
+        Is the temperature potential? If set to None, infer it from attributes.
+    clabel: bool
+        Add labels to density contours
+    clabel_kwargs: dict, None
+        Parameters that are passed to :func:`~matplotlib.pyplot.clabel`.
+    colorbar: bool, None
+        Should we add the colorbar? If set to None, check if scatter plot color is data array.
+    colorbar_kwargs: dict, None
+        Parameters that are passed to :func:`~matplotlib.pyplot.colorbar`.
+    contour_kwargs: dict, None
+        Parameters that are passed to :func:`~matplotlib.pyplot.contour`.
+    axes: None
+        Matplotlib axes instance
+
+    See also
+    --------
+    :mod:`gsw.density`
+    :mod:`gsw.conversions`
+
+    Return
+    ------
+    dict
+        With the following key, depending on what is plotted:
+        `axes`, `scatter`, `colorbar`, `contour`, `clabel`.
+
+
+    Example
+    -------
+    .. ipython:: python
+
+        @suppress
+        import numpy as np, xarray as xr, xoa, xoa.coords, cmocean
+        @suppress
+        from xoa.plot import plot_ts
+
+        # Load the CROCO meridional section
+        ds = xoa.open_data_sample("croco.south-africa.meridional.nc").isel(xi_rho=slice(30))
+        temp = ds.xoa.get('temp')
+        sal = ds.xoa.get('sal')
+        depth = xoa.coords.get_depth(ds)
+
+        # Plot
+        @savefig api.plot.plot_ts.png
+        plot_ts(temp, sal, potential=True, scatter_c=depth, contour_linewidths=0.2, clabel_fontsize=8)
+
+    """
+
+    # Potential temperature
+    cfspecs = xcf.get_cf_specs(temp)
+    # potential = POTENTIAL[potential]
+    if potential is None:
+        potential = cfspecs.match_data_var(temp, "ptemp")
+    if not potential:
+        import gsw
+
+        lat = xcoords.get_lat(temp)
+        depth = xcoords.get_depth(temp)
+        lat, depth = xr.broadcast(lat, depth)
+        pres = gsw.p_from_z(depth, lat)
+        temp = gsw.pt_from_t(sal, temp, pres)
+
+    # Init plot
+    if axes is None:
+        axes = plt.gca()
+    out = {"axes": axes}
+
+    # Scatter plot
+    scatter_kwargs = xmisc.dict_filter(
+        kwargs, "scatter_", defaults={"s": 10}, **(scatter_kwargs or {})
+    )
+    out["scatter"] = axes.scatter(sal.values, temp.values, **scatter_kwargs)
+
+    # Colorbar
+    if colorbar is None:
+        colorbar = "c" in scatter_kwargs and hasattr(scatter_kwargs["c"], "data")
+    if colorbar:
+        colorbar_kwargs = xmisc.dict_filter(
+            kwargs, "colorbar_", defaults={}, **(colorbar_kwargs or {})
+        )
+        c = scatter_kwargs["c"]
+        if "label" not in colorbar_kwargs and hasattr(c, "attrs"):
+            label = c.attrs.get("long_name") or c.name
+            if label:
+                label = label.title()
+                units = c.attrs.get("units")
+                if units:
+                    label = f"{label} [{units}]"
+                colorbar_kwargs["label"] = label
+        out["colorbar"] = plt.colorbar(out["scatter"], ax=axes, **colorbar_kwargs)
+
+    # Labels
+    axes.set_xlabel(sal.attrs.get("long_name", "Salinity").title())
+    tlabel = temp.attrs.get("long_name", "Temperature").title()
+    tunits = temp.attrs.get("units", "°C")
+    axes.set_ylabel(f"{tlabel} [{tunits}]")
+
+    # Density contours
+    if dens is not False:
+
+        import gsw
+
+        # Density as sigma0
+        (smin, tmin), (smax, tmax) = axes.viewLim.get_points()
+        tt = np.linspace(tmin, tmax, 100)
+        ss = np.linspace(smin, smax, 100)
+        ss, tt = np.meshgrid(ss, tt)
+        dd = gsw.sigma0(ss, tt)
+
+        # Contours
+        contour_kwargs = xmisc.dict_filter(
+            kwargs, "contour_", defaults={"colors": ".3"}, **(contour_kwargs or {})
+        )
+        out["contour"] = axes.contour(ss, tt, dd, **contour_kwargs)
+
+        # Contour labels
+        clabel_kwargs = xmisc.dict_filter(kwargs, "clabel_", defaults={}, **(clabel_kwargs or {}))
+        out["clabel"] = axes.clabel(out["contour"], **clabel_kwargs)
+
+    return out
