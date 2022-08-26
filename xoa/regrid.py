Fix broadcasting in 1d interpolation routines (#70)

xoa/regrid.py
@@ -86,6 +86,7 @@ class extrap_modes(misc.IntEnumChoices, metaclass=misc.DefaultEnumMeta):
 
 
 def _asfloat_(arr):
+    """We need arrays that are at least 1D and that are not dates, booleans or integers"""
     arr = np.asarray(arr)
     arr = np.atleast_1d(arr)
     if arr.dtype.type is np.datetime64:
@@ -100,18 +101,29 @@ def _wrapper1d_(vari, *args, func_name, **kwargs):
 
     Output array is reshaped back accordindly
     """
+
+    # The function to call
+    func = getattr(interp, func_name)
+
     # To 2D
-    vari = _asfloat_(vari)
-    eshape = vari.shape[:-1]
-    args = [_asfloat_(arr) for arr in args]
-    args = [np.reshape(arr, (-1, arr.shape[-1])) for arr in [vari] + args]
+    args = [vari] + list(args)
+    eshapes = []
+    for arr in args:
+        eshape = list(arr.shape[:-1])
+        if len(eshapes) and len(eshape) < len(eshapes[-1]):
+            eshape = [1] * (len(eshapes[-1]) - len(eshape)) + eshape
+        eshapes.append(eshape)
+    eshapes = np.array(eshapes, dtype='l')
+    args = [_asfloat_(arr).reshape(-1, arr.shape[-1]) for arr in args]
+    func_code = getattr(func, "func_code", getattr(func, "__code__"))
+    if "eshapes" in func_code.co_varnames[: func_code.co_argcount]:
+        args = args + [eshapes]
 
     # Call
-    func = getattr(interp, func_name)
     varo = func(*args, **kwargs)
 
     # From 2D
-    return varo.reshape(eshape + varo.shape[-1:])
+    return varo.reshape(tuple(eshapes.max(axis=0)) + varo.shape[-1:])
 
 
 def regrid1d(
@@ -208,7 +220,7 @@ def regrid1d(
     # Input coordinate
     if coord_in_name:
         assert coord_in_name in da.coords, 'Invalid coordinate'
-        coord_in = da.coords['coord_in_name']
+        coord_in = da.coords[coord_in_name]
     else:
         coord_in = cfspecs_in.search_coord_from_dim(da, dim_in, errors="raise")
         coord_in_name = coord_in.name

xoa/tests/test_interp.py
@@ -23,14 +23,21 @@ def round_as_time(arr, units="us", origin="1950-01-01"):
 
 
 @functools.lru_cache()
-def get_interp1d_data(yimin=-100.0, yimax=0.0, yomin=-90.0, yomax=10.0, irregular=True):
+def get_interp1d_data(
+    yimin=-100.0,
+    yimax=0.0,
+    yomin=-90.0,
+    yomax=10.0,
+    irregular=True,
+    nx=17,
+    nyi=15,
+    nyo=25,
+    mask=True,
+):
 
     np.random.seed(0)
 
     # coords
-    nx = 17
-    nyi = 15
-    nyo = 25
     yi = np.linspace(yimin, yimax, nyi)
     yo = np.linspace(yomin, yomax, nyo)
     x = np.linspace(0, 700, nx)
@@ -47,20 +54,24 @@ def get_interp1d_data(yimin=-100.0, yimax=0.0, yomin=-90.0, yomax=10.0, irregula
     # input
     xxi = np.resize(x, (nyi, nx)).T
     vari = vfunc(y=yyi, x=xxi)
-    vari[int(nx / 3) : int(2 * nx / 3), int(nyi / 3) : int(2 * nyi / 3)] = np.nan
+    if mask:
+        vari[int(nx / 3) : int(2 * nx / 3), int(nyi / 3) : int(2 * nyi / 3)] = np.nan
 
-    return xxi, yyi, vari, xxo, yyo
+    # shapes of extra dims
+    eshapes = np.vstack((vari.shape[:-1], yyi.shape[:-1], yyo.shape[:-1]))
+
+    return xxi, yyi, vari, xxo, yyo, eshapes
 
 
 def test_interp_nearest1d():
 
     # Get data
-    xxi, yyi, vari, xxo, yyo = get_interp1d_data()
+    xxi, yyi, vari, xxo, yyo, eshapes = get_interp1d_data()
 
     # Interpolation
-    varon = interp.nearest1d(vari, yyi, yyo)
-    yyon = interp.nearest1d(yyi, yyi, yyo)
-    xxon = interp.nearest1d(xxi, yyi, yyo)
+    varon = interp.nearest1d(vari, yyi, yyo, eshapes)
+    yyon = interp.nearest1d(yyi, yyi, yyo, eshapes)
+    xxon = interp.nearest1d(xxi, yyi, yyo, eshapes)
     varon_true = vfunc(y=yyon, x=xxon)
     varon_true[np.isnan(varon)] = np.nan
     np.testing.assert_allclose(varon_true, varon)
@@ -69,10 +80,10 @@ def test_interp_nearest1d():
 def test_interp_linear1d():
 
     # Get data
-    xxi, yyi, vari, xxo, yyo = get_interp1d_data()
+    xxi, yyi, vari, xxo, yyo, eshapes = get_interp1d_data()
 
     # Interpolation
-    varol = interp.linear1d(vari, yyi, yyo)
+    varol = interp.linear1d(vari, yyi, yyo, eshapes)
     assert not np.isnan(varol).all()
     varol_true = vfunc(y=yyo, x=xxo)
     varol_true[np.isnan(varol)] = np.nan
@@ -82,10 +93,10 @@ def test_interp_linear1d():
 def test_interp_cubic1d():
 
     # Get data
-    xxi, yyi, vari, xxo, yyo = get_interp1d_data()
+    xxi, yyi, vari, xxo, yyo, eshapes = get_interp1d_data()
 
     # Interpolation
-    varoh = interp.cubic1d(vari, yyi, yyo)
+    varoh = interp.cubic1d(vari, yyi, yyo, eshapes)
     assert not np.isnan(varoh).all()
     assert np.nanmax(varoh) <= np.nanmax(vari)
     assert np.nanmin(varoh) >= np.nanmin(vari)
@@ -94,10 +105,10 @@ def test_interp_cubic1d():
 def test_interp_hermit1d():
 
     # Get data
-    xxi, yyi, vari, xxo, yyo = get_interp1d_data()
+    xxi, yyi, vari, xxo, yyo, eshapes = get_interp1d_data()
 
     # Interpolation
-    varoh = interp.hermit1d(vari, yyi, yyo)
+    varoh = interp.hermit1d(vari, yyi, yyo, eshapes)
     assert not np.isnan(varoh).all()
     assert np.nanmax(varoh) <= np.nanmax(vari)
     assert np.nanmin(varoh) >= np.nanmin(vari)
@@ -107,7 +118,7 @@ def test_interp_hermit1d():
 def test_interp1d_nans_in_coords(method):
 
     # Get data
-    xxi, yyi, vari, xxo, yyo = get_interp1d_data()
+    xxi, yyi, vari, xxo, yyo, eshapes = get_interp1d_data()
 
     # Add nans to coords
     yyin = yyi.copy()
@@ -119,76 +130,65 @@ def test_interp1d_nans_in_coords(method):
 
     # Interpolations
     func = getattr(interp, method + "1d")
-    varol = func(vari[:, 3:-3], yyi[:, 3:-3], yyo[:, 3:-3])
-    varoln = func(vari, yyin, yyon)
+    varol = func(vari[:, 3:-3], yyi[:, 3:-3], yyo[:, 3:-3], eshapes)
+    varoln = func(vari, yyin, yyon, eshapes)
     np.testing.assert_allclose(varol, varoln[:, 3:-3])
 
 
-# def test_interp_cellave1d():
+@pytest.mark.parametrize("method", ["nearest", "linear", "cubic", "hermit"])
+def test_interp_interp1d_eshapes(method):
 
-#     # coords
-#     nx = 17
-#     nyi = 20
-#     nyo = 12
-#     yib = np.linspace(-1000., 0., nyi+1)
-#     yob = np.linspace(-1200, 200, nyo+1)
-#     xb = np.arange(nx+1)
-#     xxib, yyib = np.meshgrid(xb, yib)
-#     xxob, yyob = np.meshgrid(xb, yob)
+    # Get data and func
+    xxi, yyi, vari, xxo, yyo, eshapes = get_interp1d_data(nx=18, mask=False, irregular=False)
+    eshapes = np.repeat([[3, 6]], 3, axis=0)
+    func = getattr(interp, method + "1d")
 
-#     # input
-#     u, v = np.mgrid[-3:3:nx*1j, -3:3:nyi*1j]-2
-#     vari = np.asarray(u**2+v**2)
-#     vari[int(nx/3):int(2*nx/3), int(nyi/3):int(2*nyi/3)] = np.nan
+    # Reference
+    varol_ref = func(vari, yyi, yyo, eshapes).reshape(3, 6, -1)
 
-#     # conserv, no extrap
-#     varoc = interp.cellave1d(vari, yib, yob, conserv=1, extrap=0)
-#     sumi = np.nansum(vari*np.resize(np.diff(yib), vari.shape), axis=1)
-#     sumo = np.nansum(varoc*np.resize(np.diff(yob), varoc.shape), axis=1)
-#     np.testing.assert_allclose(sumi, sumo)
+    # missing dim 0 for vari
+    vari0 = vari.reshape(3, 6, -1)[0]
+    eshapes0 = eshapes.copy()
+    eshapes0[0, 0] = 1
+    varol0 = func(vari0, yyi, yyo, eshapes0).reshape(3, 6, -1)
+    np.testing.assert_allclose(varol_ref[0], varol0[0])
+    np.testing.assert_allclose(varol_ref[0], varol0[1])
 
-#     # average, no extrap
-#     varoa = interp.cellave1d(vari, yib, yob, conserv=0, extrap=0)
-#     np.testing.assert_allclose([np.nanmin(vari), np.nanmax(vari)],
-#                                [np.nanmin(vari), np.nanmax(vari)])
+    # missing dim 1 for vari
+    vari1 = vari.reshape(3, 6, -1)[:, 0]
+    eshapes1 = eshapes.copy()
+    eshapes1[0, 1] = 1
+    varol1 = func(vari1, yyi, yyo, eshapes1).reshape(3, 6, -1)
+    np.testing.assert_allclose(varol_ref[:, 0], varol1[:, 0])
+    np.testing.assert_allclose(varol_ref[:, 0], varol1[:, 1])
 
-#     # average, extrap
-#     varoe = interp.cellave1d(vari, yib, yob, conserv=0, extrap=2)
-#     assert not np.isnan(varoe[0]).any()
-#     vv = varoa[0, ~np.isnan(varoa[0])]
-#     np.testing.assert_allclose(vv[[0, -1]], varoe[0, [0, -1]])
+    # missing dim 0 for yyi
+    yyi0 = yyi.reshape(3, 6, -1)[0]
+    eshapes0 = eshapes.copy()
+    eshapes0[1, 0] = 1
+    varol0 = func(vari, yyi0, yyo, eshapes0).reshape(3, 6, -1)
+    np.testing.assert_allclose(varol_ref, varol0)
 
+    # missing dim 1 for yyi
+    yyi1 = yyi.reshape(3, 6, -1)[:, 0]
+    eshapes1 = eshapes.copy()
+    eshapes1[1, 1] = 1
+    varol1 = func(vari, yyi1, yyo, eshapes1).reshape(3, 6, -1)
+    np.testing.assert_allclose(varol_ref, varol1)
 
-# def test_interp_cellave1dx():
+    # missing dim 0 for yyo
+    yyo0 = yyo.reshape(3, 6, -1)[0]
+    eshapes0 = eshapes.copy()
+    eshapes0[2, 0] = 1
+    varol0 = func(vari, yyi, yyo0, eshapes0).reshape(3, 6, -1)
+    np.testing.assert_allclose(varol_ref, varol0)
 
-#     # coords
-#     nx = 17
-#     nyi = 20
-#     nyo = 12
-#     yib = np.linspace(-1000., 0., nyi+1)
-#     yob = np.linspace(-1200, 200, nyo+1)
-#     yyib = np.resize(yib, (nx, nyi+1))
-#     dyi = (yib[1]-yib[0])*0.49
-#     np.random.seed(0)
-#     yyib += np.random.uniform(-dyi, dyi, yyib.shape)
-
-#     # input
-#     u, v = np.mgrid[-3:3:nx*1j, -3:3:nyi*1j]-2
-#     vari = np.asarray(u**2+v**2)
-#     vari[int(nx/3):int(2*nx/3), int(nyi/3):int(2*nyi/3)] = np.nan
-
-#     # conserv, no extrap
-#     varoc = interp.cellave1dx(vari, yyib, yob, conserv=1, extrap=0)
-#     sumi = np.nansum(vari*np.diff(yyib, axis=1), axis=1)
-#     sumo = np.nansum(varoc*np.resize(np.diff(yob), varoc.shape), axis=1)
-#     np.testing.assert_allclose(sumi, sumo)
-
-#     # average, no extrap
-#     interp.cellave1dx(vari, yyib, yob, conserv=0, extrap=0)
-
-#     # average, extrap
-#     varoe = interp.cellave1dx(vari, yyib, yob, conserv=0, extrap=2)
-#     assert not np.isnan(varoe[0]).any()
+    # missing dim 1 for yyo
+    yyo1 = yyo.reshape(3, 6, -1)[:, 0]
+    eshapes1 = eshapes.copy()
+    eshapes1[2, 1] = 1
+    varol1 = func(vari, yyi, yyo1, eshapes1).reshape(3, 6, -1)
+    np.testing.assert_allclose(varol_ref, varol1)
 
 
 def test_interp_cellave1d():
@@ -207,23 +207,24 @@ def test_interp_cellave1d():
     yyob = np.resize(yob, (nx, nyo + 1))
     dyo = (yob[1] - yob[0]) * 0.49
     yyob += np.random.uniform(-dyo, dyo, yyob.shape)
+    eshapes = np.full((3, 1), nx)
 
     # input
     u, v = np.mgrid[-3 : 3 : nx * 1j, -3 : 3 : nyi * 1j] - 2
-    vari = np.asarray(u ** 2 + v ** 2)
+    vari = np.asarray(u**2 + v**2)
     vari[int(nx / 3) : int(2 * nx / 3), int(nyi / 3) : int(2 * nyi / 3)] = np.nan
 
     # conserv, no extrap
-    varoc = interp.cellave1d(vari, yyib, yyob, conserv=True, extrap="no")
+    varoc = interp.cellave1d(vari, yyib, yyob, eshapes, conserv=True, extrap="no")
     sumi = np.nansum(vari * np.diff(yyib, axis=1), axis=1)
     sumo = np.nansum(varoc * np.diff(yyob, axis=1), axis=1)
     np.testing.assert_allclose(sumi, sumo)
 
     # average, no extrap
-    interp.cellave1d(vari, yyib, yyob, conserv=0, extrap="no")
+    interp.cellave1d(vari, yyib, yyob, eshapes, conserv=0, extrap="no")
 
     # average, extrap
-    varoe = interp.cellave1d(vari, yyib, yyob, conserv=False, extrap="both")
+    varoe = interp.cellave1d(vari, yyib, yyob, eshapes, conserv=False, extrap="both")
     assert not np.isnan(varoe[0]).any()
 
 

xoa/tests/test_regrid.py
@@ -14,7 +14,7 @@ from test_interp import get_grid2locs_coords, vfunc, get_interp1d_data
 def test_regrid_regrid1d():
 
     # Get some data
-    xxi, yyi, vari, xxo, yyo = get_interp1d_data()
+    xxi, yyi, vari, xxo, yyo, eshapes = get_interp1d_data()
 
     # Some inits
     nz1 = xxo.shape[1]
@@ -54,10 +54,17 @@ def test_regrid_regrid1d():
     assert not np.isnan(da_out).all()
 
     # same but transposed
-    da_in = da_in.transpose("time", "nz", "lon")
-    da_out = regrid.regrid1d(da_in, depth_out, method="linear")
-    assert da_out.dims == ('time', "nk", "lon")
-    assert not np.isnan(da_out).all()
+    da_in_t = da_in.transpose("time", "nz", "lon")
+    da_out_t = regrid.regrid1d(da_in_t, depth_out, method="linear")
+    assert da_out_t.dims == ('time', "nk", "lon")
+    assert not np.isnan(da_out_t).all()
+
+    # now we remove/add some dims
+    depth_in_ed = da_in_t.depth.broadcast_like(da_in_t).isel(lon=0).drop("lon")
+    da_in_ed = da_in_t.assign_coords({"depth": depth_in_ed})
+    da_out_ed = regrid.regrid1d(da_in_ed, depth_out, method="linear")
+    assert da_out_ed.dims == ('time', "nk", "lon")
+    np.testing.assert_allclose(da_out_ed.isel(lon=0), da_out_t.isel(lon=0))
 
 
 def test_regrid_regrid1d_time():
@@ -84,7 +91,6 @@ def test_regrid_regrid1d_time():
 )
 def test_regrid_extrap1d(mode, expected):
     nz, ny, nx = 4, 3, 5
-    zi = np.linspace(3, 10, nz)
     zi = xr.DataArray(np.arange(nz), dims="z")
     vi = xr.DataArray(np.ones((nz, ny, nx)), dims=('z', 'y', 'x'), coords={"z": zi})
     vi[:, 0] = np.nan