6b69ebc455ce9c7e725f07bddc4b4b51e14f0302
Stephane Raynaud <stephane.raynaud@shom.fr>
2024-11-20 14:17:04 +0100

    Use pyproject.toml instead of setup.cfg

--- setup.py
+++ setup.py
@@ -34,10 +34,4 @@
 from setuptools import setup
 
 
-with open('README.rst') as f:
-    long_description = f.read()
-
-setup(
-      long_description=long_description,
-      use_scm_version={"fallback_version": "999"}
-      )
+setup()  # use_scm_version={"fallback_version": "999"})
